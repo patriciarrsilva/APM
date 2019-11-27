2e390026d35f649f76dc9b30cbeded811394fdb1
Patricia Silva <Patricia.Silva@ctw.bmwgroup.com>
2019-11-27 16:24:35 +0000

    Registering the Service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { IProduct } from './product';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class ProductService {
 
     getProducts(): IProduct[] {
