1a5841f2a8132486c31efb4af9114b8b567aa3b2
Patricia Silva <Patricia.Silva@ctw.bmwgroup.com>
2019-11-28 17:09:14 +0000

    Exception Handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
@@ -13,6 +14,27 @@ export class ProductService {
     constructor(private http: HttpClient) {}
 
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl);
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+          tap(data => console.log('All: ' + JSON.stringify(data))),
+          catchError(this.handleError)
+        );
+    }
+
+    // When a member is marked private, it cannot be accessed from outside of its containing class
+    private handleError(err: HttpErrorResponse) {
+      // In a real world app, we may send the server to some remote logging infrastructure instead of just logging it to the console
+      let errorMessage = '';
+
+      if (err.error instanceof ErrorEvent) {
+        // A client-side or network error occurred - handle it accordingly
+        errorMessage = `An error ocurred: ${err.error.message}`;
+      } else {
+        // The backend returned an unsuccessful response code - the response body may contain clues as to what went wrong
+        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      }
+
+      console.error(errorMessage);
+
+      return throwError(errorMessage);
     }
 }
