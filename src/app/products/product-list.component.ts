d9469f7bb55adcf3eb4ab100dc1c311043690200
Patrícia <prrs89@gmail.com>
2019-12-04 22:31:11 +0000

    Filtering a List - remove comments

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -15,26 +15,18 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
     errorMessage: string;
 
-    // The getter/setter cannot have the name that matches the property name
-    // The general pattern is to name the internal properties just like a setter/getter only with the edition of an underscore
-    // The property getter and setter work just like the simple property
-    // When the data binding needs the value, it will call the getter and get the value
-    // Every time the user modifies the value, the data binding calls the setter, passing in the changed value
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
     }
     set listFilter(value: string) {
         this._listFilter = value;
-        // if there is a _listFilter value, performFilter(); is there is no filter, filteredProducts is the entire set of products
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
     filteredProducts: IProduct[];
     products: IProduct[] = [];
 
-    // The class constructor is a function that is executed when the component is first initialized
-    // The best place to set default values for more complex properties is in the class constructor
     constructor(private productService: ProductService) {
 
     }
@@ -45,7 +37,6 @@ export class ProductListComponent implements OnInit {
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
-        // .indexOf() returns -1 if the searchValue is not found; !== -1 means it was found and passes the filter
         return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
