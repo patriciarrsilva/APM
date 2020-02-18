b09197b39e8eb21851e0eb56a437624cf0a9f7c1
Patrícia <prrs89@gmail.com>
2020-02-18 23:14:52 +0000

    Gets product detail information by id

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,23 +11,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private productService: ProductService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": id,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    }
+
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void {
